edae88abf0225ec8528a96cca517e690fad56539
LXY <LXY@ycu.com>
2021-06-10 22:16:22 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author 李晓扬
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/6/10 22:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是git测试");
+    }
+}
